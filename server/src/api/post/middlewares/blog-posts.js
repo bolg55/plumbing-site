7da0679b50ff4f03da64343e296ac46d55e4b88e
bolg55 <bolger.kellen@gmail.com>
2023-06-01 11:17:23 -0400

    update strapi schema

--- server/src/api/post/middlewares/blog-posts.js
+++ server/src/api/post/middlewares/blog-posts.js
@@ -11,7 +11,7 @@ module.exports = (config, { strapi }) => {
         fields: ["name", "alternativeText", "caption", "url"],
       },
       authors: {
-        fields: ["username"],
+        fields: ["firstname"],
         populate: {
           role: {
             fields: ["name"],
