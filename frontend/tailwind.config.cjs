91fe9d329166d586c8921b16d1a05a82fbda4017
bolg55 <bolger.kellen@gmail.com>
2023-06-01 11:19:33 -0400

    update for customization

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -5,8 +5,16 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      container: {
+        center: true,
+      },
       fontFamily: {
         sans: ['Montserrat var', 'Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'almost-white': 'hsl(0, 0%, 98%)',
+        'almost-black': 'hsl(0, 0%, 8%)',
+        'medium-gray': 'hsl(0, 0%, 41%)',
       },
     },
   },
