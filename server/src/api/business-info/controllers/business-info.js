fe0628c692c4e341c31efa5ffb271ec9895f956e
bolg55 <bolger.kellen@gmail.com>
2023-06-01 22:49:25 -0400

    remove server

--- server/src/api/business-info/controllers/business-info.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * business-info controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::business-info.business-info');
--- server/src/api/business-info/services/business-info.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * business-info service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::business-info.business-info');
