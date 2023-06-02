c1d4447b56139cceef60bbd4fcda911c0c92717a
bolg55 <bolger.kellen@gmail.com>
2023-06-01 21:59:35 -0400

    fix typo

--- frontend/src/lib/services.ts
+++ frontend/src/lib/services.ts
@@ -2,7 +2,7 @@ export const services = [
   {
     name: 'Service',
     icon: 'images/service.svg',
-    href: 'servce',
+    href: 'service',
   },
   {
     name: 'Renovations',
@@ -23,5 +23,23 @@ export const services = [
     name: 'Gas',
     icon: 'images/gas.svg',
     href: 'gas',
+  },
+];
+
+export const serviceSnippets = [
+  {
+    name: 'Professional Plumbing Services',
+    href: 'service',
+    desc: "Renovating your bathroom? Our skilled technicians can install faucets and other fixtures with precision. Need a new water heater or dealing with a temperamental shower valve? We've got you covered. From gas piping and water softeners to in-floor heating systems, we have the experience and equipment to handle all your general plumbing concerns.",
+  },
+  {
+    name: 'Drain Cleaning Expertise',
+    href: 'service',
+    desc: "Blocked drain causing havoc? Rely on our drain cleaning services to clear the way. Notice flooding in your basement or odd noises from your drainage system? We're on it. Our drain cleaning services extend to floor drains, downspout drains, and more. We're also ready to offer routine maintenance and emergency sewer cleaning services, ensuring your system works smoothly when you need it most.",
+  },
+  {
+    name: 'Dependable Plumbing Repairs',
+    href: 'service',
+    desc: "When your plumbing system hits a snag, Cooper & Adlys is just a call away. From clogged toilets and drains to troublesome faucets, our team is trained to repair and restore your plumbing system. Our services also include backflow prevention and blocked sewer repairs. Rest assured, we'll arrive on-site equipped with the necessary tools to tackle any general or emergency plumbing problem.",
   },
 ];
