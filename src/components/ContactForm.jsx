e3bea0db2432cade10ea83e2528a6650689cd279
bolg55 <bolger.kellen@gmail.com>
2023-06-02 09:07:50 -0400

    remove dark test

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -10,9 +10,9 @@ const ContactForm = () => {
       <div className='mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2'>
         <div className='relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48'>
           <div className='mx-auto max-w-xl lg:mx-0 lg:max-w-lg'>
-            <div className='absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2'>
+            <div className='absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100  ring-1 ring-gray-900/10 lg:w-1/2'>
               <svg
-                className='absolute inset-0 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]'
+                className='absolute inset-0 h-full w-full stroke-gray-200  [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]'
                 aria-hidden='true'
               >
                 <defs>
@@ -42,7 +42,7 @@ const ContactForm = () => {
             <h2 className='text-3xl font-bold tracking-tight text-medium-gray'>
               Get in touch
             </h2>
-            <p className='mt-6 text-lg leading-8 text-gray-600'>
+            <p className='mt-6 text-lg leading-8 text-gray-600 '>
               We're ready to tackle your plumbing challenges, from leaking
               faucets to pipe installations. Get in touch with us today and let
               our experienced team provide you with a quick and efficient
