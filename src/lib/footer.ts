70984e482f2d457f0ee0e741ad35753a1136f1f3
bolg55 <bolger.kellen@gmail.com>
2023-06-03 14:05:33 -0400

    refine footer

---
+++ src/lib/footer.ts
@@ -0,0 +1,74 @@
+export const footerData = [
+  {
+    title: 'Services',
+    links: [
+      {
+        href: '#',
+        label: 'Residential Plumbing',
+      },
+      {
+        href: '#',
+        label: 'Commercial Plumbing',
+      },
+      {
+        href: '#',
+        label: 'Emergency Plumbing',
+      },
+      {
+        href: '#',
+        label: 'See All Services',
+      },
+    ],
+  },
+  {
+    title: 'Resources',
+    links: [
+      {
+        href: '#',
+        label: 'Blog',
+      },
+      {
+        href: '#',
+        label: 'Plumbing Tips',
+      },
+      {
+        href: '#',
+        label: 'Maintenance Checklist',
+      },
+      {
+        href: '#',
+        label: 'Gallery',
+      },
+    ],
+  },
+  {
+    title: 'Company',
+    links: [
+      {
+        href: '#',
+        label: 'Contact Us',
+      },
+      {
+        href: '#',
+        label: 'About Us',
+      },
+      {
+        href: '#',
+        label: 'Testimonials',
+      },
+    ],
+  },
+  {
+    title: 'Legal',
+    links: [
+      {
+        href: '#',
+        label: 'Privacy Policy',
+      },
+      {
+        href: '#',
+        label: 'Terms of Service',
+      },
+    ],
+  },
+];
