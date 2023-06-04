8c30f880628b7c71420946b0574d37373aa34284
bolg55 <bolger.kellen@gmail.com>
2023-06-04 01:10:49 -0400

    add astro Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
-import prefetch from "@astrojs/prefetch";
+import prefetch from '@astrojs/prefetch';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), prefetch()]
+  integrations: [tailwind(), react(), prefetch(), image()],
+  experimental: {
+    assets: true,
+  },
+  image: {
+    service: sharpImageService(),
+  },
 });
