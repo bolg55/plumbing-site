a5f57d6009a34db87dff9ac91f7c909c850181ef
bolg55 <bolger.kellen@gmail.com>
2023-06-04 01:11:40 -0400

    add date util

---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+export const formatDate = (date: Date) => {
+  return new Date(date).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'UTC',
+  });
+};
