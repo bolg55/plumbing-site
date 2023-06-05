9f0569e29caa4458cbb4a9e8cf635acecadb7d82
bolg55 <bolger.kellen@gmail.com>
2023-06-05 01:08:07 -0400

    add service areas

---
+++ src/lib/serviceAreas.ts
@@ -0,0 +1,42 @@
+export const serviceAreas = [
+  {
+    id: 1,
+    name: 'Elmira',
+  },
+  {
+    id: 2,
+    name: 'St. Jacobs',
+  },
+  {
+    id: 3,
+    name: 'Conestogo',
+  },
+  {
+    id: 4,
+    name: 'Kitchener',
+  },
+  {
+    id: 5,
+    name: 'Waterloo',
+  },
+  {
+    id: 6,
+    name: 'Cambridge',
+  },
+  {
+    id: 7,
+    name: 'Guelph',
+  },
+  {
+    id: 8,
+    name: 'Fergus',
+  },
+  {
+    id: 9,
+    name: 'Elora',
+  },
+  {
+    id: 10,
+    name: 'And surrounding communities',
+  },
+];
