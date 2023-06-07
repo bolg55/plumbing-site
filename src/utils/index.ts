52100c784c212351c209d2587193cd234c9d88e7
bolg55 <bolger.kellen@gmail.com>
2023-06-07 00:56:39 -0400

    add types

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -27,12 +27,50 @@ export const formatBlogPosts = (
     filterOutDrafts = true,
     filterOutFuturePosts = true,
     sortByDate = true,
-    limit = undefined,
+    limit,
+  }: {
+    filterOutDrafts?: boolean;
+    filterOutFuturePosts?: boolean;
+    sortByDate?: boolean;
+    limit?: number;
   } = {}
 ) => {
   const filteredPosts = posts.reduce((acc, post) => {
-    //console.log(post);
+    const { publishedDate, isDraft } = post.data;
+
+    if (filterOutDrafts && isDraft) return acc;
+
+    if (filterOutFuturePosts && new Date(publishedDate) > new Date())
+      return acc;
+
+    // add post to the list
+    acc.push(post);
+
+    return acc;
   }, []);
+
+  // sort posts by date or randomize
+  if (sortByDate) {
+    filteredPosts.sort(
+      (
+        a: { data: { publishedDate: string | number | Date } },
+        b: { data: { publishedDate: string | number | Date } }
+      ) => {
+        const aDate = new Date(a.data.publishedDate);
+        const bDate = new Date(b.data.publishedDate);
+        aDate > bDate ? -1 : 1;
+      }
+    );
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5);
+  }
+
+  // limit the number of posts
+  if (typeof limit === 'number') {
+    return filteredPosts.slice(0, limit);
+  }
+
+  return filteredPosts;
 };
 
 export const remarkReadingTime = () => {
