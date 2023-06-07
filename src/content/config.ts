ed05e94d9adda3da599263e1b38d2825dcfc7f25
bolg55 <bolger.kellen@gmail.com>
2023-06-06 20:24:55 -0400

    set SEO best practices

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,8 +3,10 @@ import { z, defineCollection } from 'astro:content';
 const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
+    description: z
+      .string()
+      .max(155, 'For optimum SEO, the description should be 155 ch or less.'),
     author: z.string(),
-    description: z.string(),
     isDraft: z.boolean(),
     publishedDate: z.date(),
     tags: z.array(z.string()),
