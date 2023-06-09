116e86ecebe011f58914b1e731d96f554eea70b4
bolg55 <bolger.kellen@gmail.com>
2023-06-09 01:13:19 -0400

    build coming soon component

---
+++ src/components/ComingSoon.tsx
@@ -0,0 +1,10 @@
+const ComingSoon = () => {
+  return (
+    <div className='justify-center h-96 items-center align-middle flex'>
+      <h2 className='text-4xl font-semibold leading-7 text-sky-800 uppercase'>
+        Coming Soon
+      </h2>
+    </div>
+  );
+};
+export default ComingSoon;
