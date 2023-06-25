9d9a04df5de1736e86e33b2e27d711ebbdb9cec2
bolg55 <bolger.kellen@gmail.com>
2023-06-24 22:35:43 -0400

    add gallery images

--- src/lib/galleryImages.ts
+++ src/lib/galleryImages.ts
@@ -1,5 +1,21 @@
 export const galleryImages = [
   {
+    img: 'https://res.cloudinary.com/djr4sjcgh/image/upload/v1687658217/IMG_6269_ncuvzh.jpg',
+    alt: 'A new tub',
+  },
+  {
+    img: 'https://res.cloudinary.com/djr4sjcgh/image/upload/v1687658362/IMG_7930_rdcy5k.jpg',
+    alt: 'A new bathroom',
+  },
+  {
+    img: 'https://res.cloudinary.com/djr4sjcgh/image/upload/v1687658286/IMG_3396_ri1chy.jpg',
+    alt: 'Cooper & Adlys truck at job site',
+  },
+  {
+    img: 'https://res.cloudinary.com/djr4sjcgh/image/upload/v1687658456/IMG_3403_rwsqv4.jpg',
+    alt: 'Cooper & Adlys truck unloading',
+  },
+  {
     img: 'https://res.cloudinary.com/djr4sjcgh/image/upload/v1687650867/IMG_0561_t6zngj.jpg',
     alt: 'A Water Softener',
   },
@@ -18,5 +34,21 @@ export const galleryImages = [
   {
     img: 'https://res.cloudinary.com/djr4sjcgh/image/upload/v1687652483/IMG_5987_1_rwizh4.jpg',
     alt: 'A mechanical room with water heater',
+  },
+  {
+    img: 'https://res.cloudinary.com/djr4sjcgh/image/upload/v1687656843/IMG_5988_1_kirq3i.jpg',
+    alt: 'A mechanical room with water heater close up',
+  },
+  {
+    img: 'https://res.cloudinary.com/djr4sjcgh/image/upload/v1687656934/IMG_0907_xxbo67.jpg',
+    alt: 'A bank of pipes',
+  },
+  {
+    img: 'https://res.cloudinary.com/djr4sjcgh/image/upload/v1687658040/IMG_6442_1_knpzfd.jpg',
+    alt: 'An RO system',
+  },
+  {
+    img: 'https://res.cloudinary.com/djr4sjcgh/image/upload/v1687658164/IMG_3418_p2o3sy.jpg',
+    alt: 'A primer',
   },
 ];
